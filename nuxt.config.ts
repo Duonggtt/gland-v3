5ba8ac4fe9197f76d3282171e54d43078cf7d6e5
Hoàng Sơn <107675094+sonkun-game@users.noreply.github.com>
2024-07-03 18:00:38 +0700

    fix

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,16 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  modules: [
+    '@primevue/nuxt-module'
+  ],
+  primevue: {
+    usePrimeVue: true,
+    components: {
+      prefix: 'Prime',
+      include: ['Button', 'DataTable']    /* Used as <PrimeButton /> and <PrimeDataTable /> */
+  }
+  },
   css: ['~/assets/css/main.css'],
   plugins: ["~/plugins/common.ts"],
   postcss: {
--- pages/index.vue
+++ pages/index.vue
@@ -2,6 +2,9 @@
   <h1 class="text-3xl text-blue-400 font-bold underline">
     Hello World, {{name}}
   </h1>
+  <div>
+    <PrimeButton label="Check" icon="pi pi-check" ></PrimeButton>
+  </div>
 </template>
 
 <script lang="ts" setup>
